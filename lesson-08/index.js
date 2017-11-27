4ff3e0218d3d83b1631ec22ef1dc844287a3a4b3
Andrew Van Slaars <avanslaars@gmail.com>
2017-11-27 15:58:23 -0500

    updated alt and coalesce lesson

--- lesson-08/index.js
+++ lesson-08/index.js
@@ -1,24 +1,73 @@
 // alt and coalesce
-const prop = require('crocks/Maybe')
+const identity = require('crocks/combinators/identity')
+const Maybe = require('crocks/Maybe')
+const prop = require('crocks/Maybe/prop')
+const {compose, intersperse, join, split, toLower} = require('ramda')
 const safe = require('crocks/Maybe/safe')
-/*
- * Create a function that returns a Maybe (fake API call perhaps)
- * That function is only concerned with data...
- * Data can be either displayed in UI or used to create a URL slug
- * The default for the url slug (404 page or something) can be pulled
- * from a variable, so we want to make sure it's in the proper format
- * so we'll still transform it the same way...
- *
- * The coalesce version of this can use a function to get the default value
- */
+
+const isNonEmptyString = val => Boolean(val) && typeof val === 'string'
+const createSlug = compose(join(''), intersperse('-'), split(' '), toLower)
+const defaultPagename = "page not found"
 const page = {
   name: "This is a sample page"
 }
 
+
 const getPageName = (obj) => {
-  return prop('name', page)
-    .chain(safe(v => Boolean(v) && typeof v === 'string'))
+  return prop('name', obj)
+    .chain(safe(isNonEmptyString))
+}
+
+// const slug = getPageName(page)
+//   .map(createSlug)
+//   .option('page-not-found') // This requires that we know how to supply the default slug
+
+// We get the supplied default when an object doesn't have page
+// const slug = getPageName({})
+//   .map(createSlug)
+//   .option('page-not-found') // This requires that we know how to supply the default slug
+
+// We could supply an alternate value prior to transformations
+// Now, if we change the way slugs are created, we don't need to
+// update a hrad-coded string :)
+// const slug = getPageName({})
+//   .alt(Maybe.of(defaultPagename))
+//   .map(createSlug)
+//   .option('This will never show')
+
+// Now we can get the value from a function
+// The left fn can't rely on args, because there won't be any to pass from a Nothing
+// The right function will receieve the unwrapped value
+const getDefaultPageName = () => {
+  const defaultPageName = "Default Supplied by a function"
+  return defaultPageName
+}
+
+// const slug = getPageName({})
+//   .coalesce(
+//     getDefaultPageName, // use a function to get the value
+//     identity // leave the value untouched
+//   )
+//   .map(createSlug)
+//   .option('This will never show')
+
+// If the "left" was in a known state (pre-transformed), part way
+// through the transformation We could move part of that process
+// into the "right" function and it would be like mapping it
+// So let's say there is another step to make a full URL
+// And, now we have a function that will return a pre-transformed
+// default slug... we can make createSlug fn the "right" fn and
+// call the default function for the "left"
+const getDefaultPageSlug = () => {
+  return 'page-not-found'
 }
 
 const slug = getPageName(page)
-  .map(str => )
+  .coalesce(
+    getDefaultPageSlug, // use a function to get the pre-transformed value
+    createSlug // convert the value to a slug
+  )
+  .map(slug => `http://localhost/pages/${slug}`)
+  .option('http://localhost')
+
+console.log(slug) /*?*/
