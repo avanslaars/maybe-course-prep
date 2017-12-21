b2255a3658287809fcd7b9fcb28177f284e4a081
Andrew Van Slaars <avanslaars@gmail.com>
2017-12-21 08:06:50 -0500

    updated lesson 2

--- lesson-02-safe/index.js
+++ lesson-02-safe/index.js
@@ -4,55 +4,69 @@ const Maybe = require('crocks/Maybe')
 
 const inc = n => n + 1
 const dbl = n => n * 2
+const toUpper = s => s.toUpperCase()
 
 /*
- * Start with existing getMaybe fn in place and show
- * that it still leads to unintended results if passed a string
- * and the string is applied to a fn that expects a number
+ * Start with existing safeNum fn in place
+ * talk about making it more flexible...
+ * Maybe we need a string example too
  */
+const safeNum = val =>
+  typeof val === 'number' ? Maybe.Just(val) : Maybe.Nothing()
 
-const getMaybe = val =>
-  (val === null || val === undefined) ? Maybe.Nothing() : Maybe.Just(val)
+// const input = safeNum(3)
+// const result = input.map(x => console.log('running inc') || inc(x))
 
-// const safeNum = getMaybe("Uh oh")
-// const result = safeNum.map(dbl)
-// console.log(result) // Just NaN
+// console.log(result)
 
 /**
- * We could start updating our getMaybe function, but it would
- * be easier to create a general purpose function that's a little more
- * flexible. Let's create a `safe` function
- *
- * `safe` will take a predicate function and a value, if the predicate
- * evaluates to true, we wrap the value in a Just, otherwise we return
- * a Nothing
+ * Add a simple string sample
+ * toUpper util will result in an error if called with a number
+ * because it uses String.prototype.toUpperCase()
+ * safeString function, checks for string
+ * use it to map over toUpper util
  */
 
-// const safe = (pred, val) => pred(val) ? Maybe.Just(val) : Maybe.Nothing()
-// const isNumber = n => typeof n === 'number' && !isNaN(n)
+const safeString = val =>
+  typeof val === 'string' ? Maybe.Just(val) : Maybe.Nothing()
 
-// const safeNum = safe(isNumber, 5)
-// const result = safeNum.map(dbl)
-// console.log(result) // Just 10
+// const inputS = safeString(10)
+// const resultS = inputS.map(toUpper) /*?*/
 
-// const safeNum = safe(isNumber, "Test")
-// const result = safeNum.map(dbl)
-// console.log(result) // Nothing
+/**
+ * Instead of building entire functions that have to repeat this
+ * pattern, let's build a reusable function...
+ *
+ * Extract the conditionals out into predicate functions
+ */
+
+const isNumber = val => typeof val === 'number'
+const isString = val => typeof val === 'string'
+
+/**
+ * Create the `safe` function...
+ */
+
+const safe = (pred, val) =>
+  pred(val) ? Maybe.Just(val) : Maybe.Nothing()
+
+/**
+ * Duplicate the previous results with the new `safe` fn
+ */
+
+const inputS = safe(isString, 10)
+const resultS = inputS.map(toUpper) /*?*/
+
+const input = safe(isNumber, 3)
+const result = input.map(inc) /*?*/
 
 /**
  * At this point, we can pull in `safe` and the predicates we used from crocks
  */
 
-const safe = require('crocks/Maybe/safe')
-const isNumber = require('crocks/predicates/isNumber')
-
-// const safeNum = safe(isNumber, 5)
-// const result = safeNum.map(dbl)
-// console.log(result) // Just 10
-
-// const safeNum = safe(isNumber, "Test")
-// const result = safeNum.map(dbl)
-// console.log(result) // Nothing
+// const safe = require('crocks/Maybe/safe')
+// const isNumber = require('crocks/predicates/isNumber')
+// const isString = require('crocks/predicates/isString')
 
 /**
  * And show that this version of `safe` is curried
@@ -61,15 +75,15 @@ const isNumber = require('crocks/predicates/isNumber')
 const maybeNumber = safe(isNumber) // pass the pred, get a fn back...
 
 // Use that function with multiple values...
-const safeNum = maybeNumber(5)
-const safeNum2 = maybeNumber(7)
+// const safeNum = maybeNumber(5)
+// const safeNum2 = maybeNumber(7)
 
 // ... and get multiple results
-const result = safeNum.map(dbl)
-const result2 = safeNum2.map(dbl)
+// const result = safeNum.map(dbl)
+// const result2 = safeNum2.map(dbl)
 
-console.log(result) // Just 10
-console.log(result2) // Just 14
+// console.log(result) // Just 10
+// console.log(result2) // Just 14
 
 
 
